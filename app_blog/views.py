9b142973e6ff55b56c0ae8c99e3a910286fd450f
Andre-Ovien <andreovien571@gmail.com>
2025-05-03 08:38:20 +0100

    update

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def blog_list(request):
+    context={
+        
+    }
+    return render(request, 'app_blog/blog_list.html', context)
