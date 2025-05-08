318372ead6828e5cad31f87f42eb98c3d80170d7
Andre-Ovien <andreovien571@gmail.com>
2025-05-08 22:08:02 +0100

    update

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.blog_list, name="blog_list")    
+    path('',views.blog_list, name="blog_list"),
+    path('create-blog/', views.CreateBlog.as_view(), name="create_blog")    
 ]
