b43dd2192fde2504feedda1f96e6c7b2826d2056
Andre-Ovien <andreovien571@gmail.com>
2025-05-22 17:09:10 +0100

    update

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.BlogList.as_view(), name="blog_list"),
     path('create-blog/', views.CreateBlog.as_view(), name="create_blog"),
-    path('details/<slug:slug>/',views.blog_details,name="blog_details")    
+    path('details/<slug:slug>/',views.blog_details,name="blog_details"),
+    path('like/<pk>/',views.like,name="like"),
+    path('unlike/<pk>/',views.unlike,name="unlike"),
+    path('my-blogs/',views.MyBlog.as_view(),name="my_blog"),
+    path('edit-blog/<pk>/',views.UpdateBlog.as_view(),name="edit_blog")    
 ]
